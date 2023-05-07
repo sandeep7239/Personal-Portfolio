ab262392dfc23b64b9041b98fd28fe4544c49d69
Sandeep Singh <88778019+sandeep7239@users.noreply.github.com>
2023-05-07 19:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-links");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby0izoFosNn65zvQx1ZKBh4RSBmzHsztbOwq8xhce91q8rYtXJqyrP7hStSicmWcxxd/exec'
+  const form = document.getElementById("formData");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
